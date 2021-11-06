58d5a6940278916dcb0b389d09cf56042f56fb45
James Pace <jpace121@gmail.com>
2021-11-06 15:27:45 +0000

    Use check box for indices on publish.

--- src/Pub.tsx
+++ src/Pub.tsx
@@ -44,21 +44,56 @@ export function Pub() {
 function PublishCard() {
     const mqtt = useContext(MQTTContext);
 
-    const [brightnessState, changeBrightnessState] = useState("1.0");
-    const [indexState, changeIndexState] = useState("0");
+    const [indexState, changeIndexState] = useState<{[key: string]: boolean}>(
+        {
+            "index-0": false,
+            "index-1": false,
+            "index-2": false,
+            "index-3": false,
+            "index-4": false,
+            "index-5": false,
+            "index-6": false,
+            "index-7": false
+        }
+    );
     const [colorState, changeColorState] = useState("off");
+    const [brightnessState, changeBrightnessState] = useState("1.0");
 
-    let onIndexChange = (e: any) => { changeIndexState(e.target.value) };
     let onColorChange = (e: any) => { changeColorState(e.target.value) };
     let onBrightnessChange = (e: any) => { changeBrightnessState(e.target.value) };
+    let onIndexChange = (e: any) =>
+        {
+            changeIndexState( prevState => ({
+                ...prevState,
+                [e.target.id]: !prevState[e.target.id]
+            }) );
+        };
 
     let onSubmit = (e : any) =>
         {
             e.preventDefault();
-            let error = mqtt.setLed(Number(indexState), colorState, Number(brightnessState));
-            if(!error) {
-                // TODO: Handle better.
-                console.log("Failed to publish!");
+
+            let nameToNumber = new Map([
+                ["index-0", 0],
+                ["index-1", 1],
+                ["index-2", 2],
+                ["index-3", 3],
+                ["index-4", 4],
+                ["index-5", 5],
+                ["index-6", 6],
+                ["index-7", 7]
+            ]);
+
+            for(const [indexName, shouldSet] of Object.entries(indexState))
+            {
+                if(shouldSet) {
+                    let indexVal = nameToNumber.get(indexName);
+                    let error = mqtt.setLed(Number(indexVal), colorState, Number(brightnessState));
+                    if(!error) {
+                        // TODO: Handle better.
+                        console.log("Failed to publish!");
+                    }
+                }
             }
         };
 
@@ -106,18 +141,17 @@ function ColorList(props: any) {
 
 function IndexList(props: any) {
     return (
-        <FloatingLabel controlId="floatingSelect" label="LED Index">
-            <Form.Select value={props.value} onChange={props.onChange}>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-            </Form.Select>
-        </FloatingLabel>
+        <Card className="padded-card">
+            <p className="text-secondary">Index</p>
+            <Form.Check label={"0"} onChange={props.onChange} checked={props.value["index-0"]} id="index-0"/>
+            <Form.Check label={"1"} onChange={props.onChange} checked={props.value["index-1"]} id="index-1"/>
+            <Form.Check label={"2"} onChange={props.onChange} checked={props.value["index-2"]} id="index-2"/>
+            <Form.Check label={"3"} onChange={props.onChange} checked={props.value["index-3"]} id="index-3"/>
+            <Form.Check label={"4"} onChange={props.onChange} checked={props.value["index-4"]} id="index-4"/>
+            <Form.Check label={"5"} onChange={props.onChange} checked={props.value["index-5"]} id="index-5"/>
+            <Form.Check label={"6"} onChange={props.onChange} checked={props.value["index-6"]} id="index-6"/>
+            <Form.Check label={"7"} onChange={props.onChange} checked={props.value["index-7"]} id="index-7"/>
+        </Card>
     );
 }
 
